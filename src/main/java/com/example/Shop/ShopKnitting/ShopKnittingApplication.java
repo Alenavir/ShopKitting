3e8715e61fe727465d14805ef7941bfb50a335bf
Alenavir <wandererNavi@gmail.com>
2024-03-03 21:22:53 +0300

    Zero

---
+++ src/main/java/com/example/Shop/ShopKnitting/ShopKnittingApplication.java
@@ -0,0 +1,14 @@
+package com.example.Shop.ShopKnitting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopKnittingApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ShopKnittingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Shop/ShopKnitting/ShopKnittingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Shop.ShopKnitting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopKnittingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
